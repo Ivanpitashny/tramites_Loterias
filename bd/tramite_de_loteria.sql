2e2537debdc0b7bdb1703f33a690714f54175067
Ivan Pitashny <ivan.pitashny@i2t-sa.com.ar>
2024-09-10 14:04:07 -0300

    cambio tabla tipo tramites

--- bd/tramite_de_loteria.sql
+++ bd/tramite_de_loteria.sql
@@ -1,47 +1,60 @@
 -- Active: 1722358145282@@127.0.0.1@3306@tramite_de_loteria
-CREATE TABLE users (
-    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(68) NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    mail VARCHAR(70) NOT NULL,
-    telefono INT,
-    enabled tinyint NOT NULL
-);
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) NOT NULL,
+    `password` varchar(255) DEFAULT NULL,
+    `nombre` varchar(255) DEFAULT NULL,
+    `apellido` varchar(255) DEFAULT NULL,
+    `mail` varchar(255) DEFAULT NULL,
+    `telefono` int(11) DEFAULT NULL,
+    `enabled` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`)
+) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-CREATE TABLE authorities (
-  username VARCHAR(50) NOT NULL,
-  authority VARCHAR(50) NOT NULL,
-  UNIQUE KEY authorities_idx_1 (username,authority),
-  CONSTRAINT authorities_ibfk_1 FOREIGN KEY (username) REFERENCES users (username)
-);
+CREATE TABLE `authorities` (
+    `username` varchar(50) NOT NULL,
+    `authority` varchar(255) DEFAULT NULL,
+    UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
+    CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-CREATE TABLE TipoTramite(
-    tt_id INT PRIMARY KEY AUTO_INCREMENT,
-    tt_tipo VARCHAR(50)
-);
+CREATE TABLE `tipo_tramite` (
+    `tt_id` int(11) NOT NULL AUTO_INCREMENT,
+    `tt_tipo` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`tt_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-CREATE TABLE Tramite(
-    t_id INT PRIMARY KEY AUTO_INCREMENT,
-    t_estado VARCHAR(50),
-    t_fecha_inicio DATE,
-    t_fecha_fin DATE,
-    tt_id INT,
-    usr_id INT,
-    Foreign Key (tt_id) REFERENCES TipoTramite(tt_id),
-    Foreign Key (usr_id) REFERENCES users(id)
-);
+CREATE TABLE `tramite` (
+    `t_id` int(11) NOT NULL AUTO_INCREMENT,
+    `t_estado` varchar(255) DEFAULT NULL,
+    `t_fecha_inicio` date DEFAULT NULL,
+    `t_fecha_fin` date DEFAULT NULL,
+    `tt_id` int(11) DEFAULT NULL,
+    `usr_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (`t_id`),
+    KEY `tt_id` (`tt_id`),
+    KEY `usr_id` (`usr_id`),
+    CONSTRAINT `tramite_ibfk_1` FOREIGN KEY (`tt_id`) REFERENCES `tipo_tramite` (`tt_id`),
+    CONSTRAINT `tramite_ibfk_2` FOREIGN KEY (`usr_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-CREATE TABLE Historial_Actividades(
-    ha_id INT PRIMARY KEY AUTO_INCREMENT,
-    ha_operacion ENUM('Eliminar','Agregar','Editar'),    
-    ha_fecha_modificacion DATE,
-    usr_id INT,
-    t_id INT,
-    Foreign Key (usr_id) REFERENCES users(id),
-    Foreign Key (t_id) REFERENCES Tramite(t_id)
-);
+CREATE TABLE `historial_actividades` (
+    `ha_id` int(11) NOT NULL AUTO_INCREMENT,
+    `ha_operacion` enum(
+        'Eliminar',
+        'Agregar',
+        'Editar'
+    ) DEFAULT NULL,
+    `ha_fecha_modificacion` date DEFAULT NULL,
+    `usr_id` int(11) DEFAULT NULL,
+    `t_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (`ha_id`),
+    KEY `usr_id` (`usr_id`),
+    KEY `t_id` (`t_id`),
+    CONSTRAINT `historial_actividades_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `historial_actividades_ibfk_2` FOREIGN KEY (`t_id`) REFERENCES `tramite` (`t_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
 INSERT INTO users (id, username, password , nombre, apellido, mail, telefono, enabled) 
 VALUES 
